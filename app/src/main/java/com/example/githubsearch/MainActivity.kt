c134de622c0258664635f01455eab8a946545b97
Rob Hess <hessro@oregonstate.edu>
2023-02-07 15:41:51 -0800

    Add code from 2/7 lecture (part 2).

--- app/src/main/java/com/example/githubsearch/MainActivity.kt
+++ app/src/main/java/com/example/githubsearch/MainActivity.kt
@@ -4,13 +4,16 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.TextUtils
 import android.util.Log
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.githubsearch.api.GitHubService
 import com.example.githubsearch.data.GitHubRepo
 import com.example.githubsearch.data.GitHubSearchResults
+import com.google.android.material.progressindicator.CircularProgressIndicator
 import com.squareup.moshi.Moshi
 import retrofit2.Call
 import retrofit2.Callback
@@ -57,6 +60,9 @@ class MainActivity : AppCompatActivity() {
     private val gitHubService = GitHubService.create()
     private val githubRepoAdapter = GitHubRepoListAdapter()
 
+    private lateinit var searchResultsListRV: RecyclerView
+    private lateinit var errorMessageTV: TextView
+    private lateinit var loadingIndicator: CircularProgressIndicator
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -65,7 +71,10 @@ class MainActivity : AppCompatActivity() {
         val searchBoxET: EditText = findViewById(R.id.et_search_box)
         val searchBtn: Button = findViewById(R.id.btn_search)
 
-        val searchResultsListRV: RecyclerView = findViewById(R.id.rv_search_results)
+        errorMessageTV = findViewById(R.id.tv_error_message)
+        loadingIndicator = findViewById(R.id.loading_indicator)
+
+        searchResultsListRV = findViewById(R.id.rv_search_results)
         searchResultsListRV.layoutManager = LinearLayoutManager(this)
         searchResultsListRV.setHasFixedSize(true)
         searchResultsListRV.adapter = githubRepoAdapter
@@ -81,9 +90,12 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun doRepoSearch(query: String) {
+        loadingIndicator.visibility = View.VISIBLE
         gitHubService.searchRepositories(query)
             .enqueue(object : Callback<GitHubSearchResults> {
                 override fun onResponse(call: Call<GitHubSearchResults>, response: Response<GitHubSearchResults>) {
+                    loadingIndicator.visibility = View.INVISIBLE
+
                     Log.d("MainActivity", "Status code: ${response.code()}")
                     if (response.isSuccessful) {
 //                        val moshi = Moshi.Builder().build()
@@ -91,10 +103,20 @@ class MainActivity : AppCompatActivity() {
 //                        val searchResults = jsonAdapter.fromJson(response.body())
 //                        Log.d("MainActivity", "Search results: $searchResults")
                         githubRepoAdapter.updateRepoList(response.body()?.items)
+                        searchResultsListRV.visibility = View.VISIBLE
+                        errorMessageTV.visibility = View.INVISIBLE
+                    } else {
+                        errorMessageTV.text = "Error: ${response.errorBody()?.string()}"
+                        searchResultsListRV.visibility = View.INVISIBLE
+                        errorMessageTV.visibility = View.VISIBLE
                     }
                 }
 
                 override fun onFailure(call: Call<GitHubSearchResults>, t: Throwable) {
+                    errorMessageTV.text = "Error: ${t.message}"
+                    loadingIndicator.visibility = View.INVISIBLE
+                    searchResultsListRV.visibility = View.INVISIBLE
+                    errorMessageTV.visibility = View.VISIBLE
                     Log.d("MainActivity", "Error making API call: ${t.message}")
                 }
             })
